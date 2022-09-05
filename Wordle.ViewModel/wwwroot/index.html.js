373c353183ce5e14703eeb28d5ed6189fc26f70c
faeriephae <73592659+faeriephae@users.noreply.github.com>
2022-09-05 07:32:12 +0200

    added key component

--- Wordle.ViewModel/wwwroot/index.html.js
+++
@@ -1,8 +0,0 @@
-//Focus Square/letter input
-var setFocus = function getFocus(e) {
-    document.getElementById(e.id).focus();
-};
-var setBreak = function insertBreak(e) {
-    document.getElementById(e.id).append("<br>");
-};
-//# sourceMappingURL=index.html.js.map
